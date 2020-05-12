0667f07168ad5da99d3a79a9f9284e7ed5291d9e
Naomi Tal Tsabari <37747268+naomital@users.noreply.github.com>
2020-05-13 01:52:28 +0300

    Enam, blood , game over and start butten
    Enam, blood , game over and start butten

---
+++ maze/Assets/Scripts/playerControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour
+{
+    public GameObject GameOverText, restartButton, blood;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameOverText.SetActive(false);
+        restartButton.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag.Equals("Enemy"))
+        {
+            Instantiate(blood, transform.position, Quaternion.identity);
+            GameOverText.SetActive(true);
+            restartButton.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}
