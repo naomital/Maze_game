709af44924dd2167a10ad95b3243f9ebae494b43
Michal Yelkin <michalkin9@gmail.com>
2020-05-13 13:49:36 +0300

    update standing animation

--- maze/Assets/Scripts/KeyboardMover.cs
+++ maze/Assets/Scripts/KeyboardMover.cs
@@ -15,15 +15,38 @@ public class KeyboardMover: MonoBehaviour {
         animator = GetComponent<Animator>();
     }
     void Update() {
-        float horizontal = Input.GetAxis("Horizontal"); // +1 if right arrow is pushed, -1 if left arrow is pushed, 0 otherwise
-        float vertical = Input.GetAxis("Vertical");     // +1 if up arrow is pushed, -1 if down arrow is pushed, 0 otherwise
-        Vector3 movementVector = new Vector3(horizontal, vertical, 0) * speed * Time.deltaTime;
-        transform.position += movementVector;
-        //transform.Translate(movementVector);
-        // NOTE: "Translate(movementVector)" uses relative coordinates - 
-        //       it moves the object in the coordinate system of the object itself.
-        // In contrast, "transform.position += movementVector" would use absolute coordinates -
-        //       it moves the object in the coordinate system of the world.
-        // It makes a difference only if the object is rotated.
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            animator.SetBool("isWalking", true);
+            
+
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+
+            transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+            animator.SetBool("isWalking", true);
+
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+            animator.SetBool("isWalking", true);
+
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0, -speed * Time.deltaTime, 0);
+            animator.SetBool("isWalking", true);
+
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+
+        }
     }
 }
